eb8fee5cedd264213e53aa08534a9efc2bf610fd
prachi1025 <prachipargal123@gmail.com>
2025-03-14 02:26:49 +0530

    remove deprecated methods

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to MongoDB Atlas successfully!");
 }).catch((err) => {
     console.error("MongoDB connection error:", err);
